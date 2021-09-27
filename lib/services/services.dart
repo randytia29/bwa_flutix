cd8727136677d2df3685135ac6f98d0485405234
Randytia Akbar <randytia.akbar@gmail.com>
2021-09-27 15:47:24 +0700

    delete old movie

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,21 +1,14 @@
-import 'dart:convert';
-
-import 'package:bwaflutix/models/movie.dart';
-
 import '../main.dart';
 import '../models/models.dart';
-import '../shared/shared.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:http/http.dart' as http;
 import '../extensions/extensions.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 
 part 'auth_services.dart';
 part 'flutix_transaction_services.dart';
-part 'movie_services.dart';
 part 'ticket_services.dart';
 part 'user_services.dart';
 part 'notification_service.dart';
